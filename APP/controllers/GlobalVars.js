919e03fd28f839262c95d84346b5041428ba6b6c
Alexis Burgos <alexisburgosgtm@outlook.com>
2020-09-03 13:05:25 -0600

    cambio de spiner

--- APP/controllers/GlobalVars.js
+++ APP/controllers/GlobalVars.js
@@ -4,8 +4,9 @@ let GlobalToken = '';
 let GlobalUser = '';
 let GlobalWaitElement = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>';
 let GlobalLoader = `<div class="spinner-border text-danger" role="status"><span class="sr-only">Loading...</span></div>
-                    <div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>
-                    <div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>`;
+                    <div class="spinner-grow text-info" role="status"><span class="sr-only">Loading...</span></div>
+                    <div class="spinner-grow text-warning" role="status"><span class="sr-only">Loading...</span></div>
+                    <div class="spinner-grow text-success" role="status"><span class="sr-only">Loading...</span></div>`;
 
 
 
