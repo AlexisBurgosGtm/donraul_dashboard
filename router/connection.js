921f5d648b79b0b8bbddcb0798266cf01a9e48e8
Alexis Burgos <alexisburgosgtm@outlook.com>
2020-09-11 11:51:02 -0600

    cambio de host 2

--- router/connection.js
+++ router/connection.js
@@ -8,10 +8,10 @@
 
 
 const config = {
-	user: 'DB_A6478C_mercadosefectivos_admin',
+	user: 'DB_A6478C_donraul_admin',
 	password: 'razors1805',
 	server: 'sql5006.site4now.net',
-	database: 'DB_A6478C_mercadosefectivos',
+	database: 'DB_A6478C_donraul',
 	pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}
 };
 
